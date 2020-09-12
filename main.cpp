36ccb63960e519cfb018560fc0c7fc7c27cb8b6a
Accacio <raccacio@poli.ufrj.br>
2020-09-12 16:41:23 +0200

    transparent window

--- main.cpp
+++ main.cpp
@@ -5,22 +5,33 @@
 
 int main(int argc, char *argv[]) {
     Display* display = XOpenDisplay(NULL);
-    unsigned long borderColor, backgroundColor;
+
+    XVisualInfo vinfo;
+    XMatchVisualInfo(display, DefaultScreen(display), 32, TrueColor, &vinfo);
+
+    XSetWindowAttributes attr;
+    attr.colormap = XCreateColormap(display, DefaultRootWindow(display), vinfo.visual, AllocNone);
     /* color = aa rr gg bb */
-    borderColor = 0x00ffffff;
-    backgroundColor = 0x00000000;
-    Window window = XCreateSimpleWindow(display,
-                                        XDefaultRootWindow(display),
-                                        10, 10, 300, 300, 0,
-                                        borderColor, backgroundColor);
+    attr.border_pixel = 0x00000000;
+    attr.background_pixel = 0x00000010;
 
-    Atom WM_DELETE_WINDOW = XInternAtom(display, "WM_DELETE_WINDOW", False);
-    XSetWMProtocols(display, window, &WM_DELETE_WINDOW, 1);
-    GC gc=XCreateGC(display, window, 0,0);
+    Window window = XCreateWindow(display,
+                                  XDefaultRootWindow(display),
+                                  10, 10, 300, 300, 0,
+                                  vinfo.depth, InputOutput,
+                                  vinfo.visual,
+                                  CWColormap | CWBorderPixel | CWBackPixel,
+                                  &attr);
 
-    XClearWindow(display, window);
+
+
+    // XClearWindow(display, window);
+    XMoveResizeWindow(display, window, 100, 100, 400, 400);
+    XSetWindowBorderWidth(display, window, 1);
+
+    // XMoveResizeWindow(display,window,0,0,640,480);
+    // XMapWindow(display, window);
     XMapRaised(display, window);
-
     XEvent event;
     while(True) {
         XNextEvent(display, &event);
