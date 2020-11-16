b84911ca31a32a1a5b6f908b8db34b0cfc3df705
Accacio <raccacio@poli.ufrj.br>
2020-11-16 21:39:10 +0100

    two VAO one for camera and other for cube

--- main.cpp
+++ main.cpp
@@ -22,6 +22,36 @@
 #include <time.h>
 #include <errno.h> 
 
+
+static unsigned long last_time;
+
+
+unsigned long get_nanos() {
+    struct timespec ts;
+    timespec_get(&ts, TIME_UTC);
+    return (long)ts.tv_sec * 1000000000L + ts.tv_nsec;
+}
+
+unsigned long get_milis() {
+    return (long)get_nanos()/ 1000000L;
+}
+static void init_fps() {
+    last_time= get_nanos();
+}
+static void print_fps() {
+    unsigned long t;
+    unsigned long dt;
+    static unsigned long nframes = 0;
+    nframes++;
+    t = get_nanos();
+    dt = t - last_time;
+    if (dt > 250000000) {
+        printf("FPS = %f\n", (nframes / (dt / 1000000000.0)));
+        last_time= t;
+        nframes = 0;
+    }
+}
+
 int msleep(long msec)
 {
     struct timespec ts;
@@ -94,12 +124,7 @@ float cubeVertices[] = {
 -0.5f,  0.5f, -0.5f,  0.0f, 1.0f , 0.0f,0.0f
 };
 
-unsigned int indices[] = {
-0,1,2,2,3,0
-// 0,1,2,3 //Quads
-};
-
-const char *vertexShaderSource = "#version 330 core\n"
+const char *cubeVertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "layout (location = 1) in vec4 aColor;\n"
     // "layout (location = 1) in vec2 aTexCoord;\n"
@@ -116,7 +141,7 @@ const char *vertexShaderSource = "#version 330 core\n"
     // "   TexCoord = aTexCoord;\n"
     "}\n";
 
-const char *fragmentShaderSource = "#version 330 core\n"
+const char *cubeFragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
     "in vec4 ourColor;\n"
     // "in vec2 TexCoord;\n"
@@ -128,12 +153,83 @@ const char *fragmentShaderSource = "#version 330 core\n"
     "    FragColor = ourColor;\n"
     // "    FragColor = vec4(1.0f,0.5f,0.2f,1.0f);\n"
     "}";
-float clockToMilliseconds(clock_t ticks){
-    // units/(units/time) => time (seconds) * 1000 = milliseconds
-    float res =  ((float) ticks/CLOCKS_PER_SEC)*1000;
-    // printf("%f ms\n",res);
-    return res;
+
+
+
+float cameraVertices[] = {
+/*  xy            uv */
+    1.0,  1.0,   0.0, 1.0,
+     1.0,  -1.0,   0.0, 0.0,
+     -1.0, -1.0,   1.0, 0.0,
+    -1.0, 1.0,   1.0, 1.0,
+};
+unsigned int cameraIndices[] = {
+    0, 1, 3,
+    1, 2, 3,
+};
+
+const char *cameraVertexShaderSource = "#version 330 core\n"
+    "in vec2 coord2d;\n"
+    "in vec2 vertexUv;\n"
+    "out vec2 fragmentUv;\n"
+    "void main() {\n"
+    "    gl_Position = vec4(coord2d, -1.0f, 1.0f);\n"
+    "    fragmentUv = vertexUv;\n"
+    "}\n";
+
+const char *cameraFragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "in vec2 fragmentUv;\n"
+    // "uniform sampler2D ourTexture;\n"
+    "void main()\n"
+    "{\n"
+    // "    FragColor = texture(ourTexture,TexCoord);\n"
+    "    FragColor = vec4(0.5f,0.7f,1.0f,1f);\n"
+    // "    FragColor = vec3(0.5f,0.2f,1.0f);\n"
+    "}";
+
+unsigned int getProgram(const char*vertexShaderSource, const char*fragmentShaderSource){
+    unsigned int vertexShader,fragmentShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+    int  success;
+
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        printf("Error Shader compilation %s\n",infoLog);
+    }
+
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        printf("Error Shader compilation %s\n",infoLog);
+    }
+
+
+    unsigned int id;
+    id = glCreateProgram();
+    glAttachShader(id, vertexShader);
+    glAttachShader(id, fragmentShader);
+    glLinkProgram(id);
+    glGetProgramiv(id, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(id, 512, NULL, infoLog);
+        printf("Error Shader link %s\n",infoLog);
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    return id;
 }
+
+
 
 int main(int argc, char *argv[]) {
     Display* display = XOpenDisplay(NULL);
@@ -184,12 +280,13 @@ int main(int argc, char *argv[]) {
 
     XSetWindowAttributes attr;
     attr.colormap = XCreateColormap(display, DefaultRootWindow(display), visual->visual, AllocNone);
-    // attr.colormap = XCreateColormap(display, DefaultRootWindow(display), vinfo.visual, AllocNone);
 
     attr.border_pixel = 0x00000000;
     attr.background_pixel = 0x00000000;
+
     // Should respond to exposure(Redraw) and keypresses
     attr.event_mask = ExposureMask | KeyPressMask;
+    // attr.event_mask = KeyPressMask;
     Window window = XCreateWindow(display,
                                   xroot,
                                   10, 10, 500, 500,
@@ -238,72 +335,59 @@ int main(int argc, char *argv[]) {
 
     // glXcontext= glXCreateNewContext(display, pvinfo, GLX_RGBA_TYPE, 0, True);
     // glXMakeCurrent(display, window, glXcontext);
+    unsigned int idCube = getProgram(cubeVertexShaderSource, cubeFragmentShaderSource);
+    unsigned int idCamera = getProgram(cameraVertexShaderSource, cameraFragmentShaderSource);
 
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-    glCompileShader(vertexShader);
-    int  success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if(!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        printf("Error Shader compilation %s\n",infoLog);
-    }
+    // glUseProgram(idCube);
 
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if(!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        printf("Error Shader compilation %s\n",infoLog);
-    }
+    unsigned int cameraVAO,cubeVAO,cameraVBO,VBO,cameraEBO;
 
 
-    unsigned int id;
-    id = glCreateProgram();
-    glAttachShader(id, vertexShader);
-    glAttachShader(id, fragmentShader);
-    glLinkProgram(id);
-    glGetProgramiv(id, GL_LINK_STATUS, &success);
-    if(!success) {
-        glGetProgramInfoLog(id, 512, NULL, infoLog);
-        printf("Error Shader link %s\n",infoLog);
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-    glUseProgram(id);
 
-    unsigned int VAO,VBO,EBO;
-    glGenVertexArrays(1, &VAO);
+
     glGenBuffers( 1, &VBO );
-    glGenBuffers( 1, &EBO );
-
-    glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-    // glBufferData(GL_ARRAY_BUFFER, sizeof(verticesPositionColorTexture), verticesPositionColorTexture, GL_STATIC_DRAW);
+
+    glGenVertexArrays(1, &cubeVAO);
+    glBindVertexArray(cubeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // Bind data to buffer
     glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices , GL_STATIC_DRAW);
-    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // Binding indice array to elementbuffer
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // vertice position
+    // vertex position
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
-    glEnableVertexArrayAttrib(VAO, 0);
-
-    // glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
-    // glEnableVertexArrayAttrib(VAO, 1);
-
-    // color
+    glEnableVertexArrayAttrib(cubeVAO, 0);
+    // vertex color
     glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexArrayAttrib(VAO, 1);
+    glEnableVertexArrayAttrib(cubeVAO, 1);
+    glBindVertexArray(0);
+
+
+
+    glGenBuffers( 1, &cameraVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, cameraVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cameraVertices), cameraVertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glGenBuffers( 1, &cameraEBO );
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cameraEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cameraIndices), cameraIndices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    unsigned int coord2d_location = glGetAttribLocation(idCamera, "coord2d");
+    unsigned int vertexUv_location = glGetAttribLocation(idCamera, "vertexUv");
+	// unsigned int myTextureSampler_location = glGetUniformLocation(idCamera, "myTextureSampler");
+	// unsigned int pixD_location = glGetUniformLocation(idCamera, "pixD");
+
+    glGenVertexArrays(1, &cameraVAO);
+    glBindVertexArray(cameraVAO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cameraEBO);
+    glBindBuffer(GL_ARRAY_BUFFER, cameraVBO);
+    glVertexAttribPointer(coord2d_location, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (GLvoid*)0);
+    glEnableVertexAttribArray(coord2d_location);
+    glVertexAttribPointer(vertexUv_location, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (GLvoid*)(2 * sizeof(float)));
+    glEnableVertexAttribArray(vertexUv_location);
+    glBindVertexArray(0);
 
     float uptime;
     XEvent event;
@@ -318,8 +402,9 @@ int main(int argc, char *argv[]) {
     struct timeval endFrame;
     long int ms, elapsed=0;
 
-    while(True) {
 
+    init_fps();
+    while(True) {
         // Event Loop
         while (XPending(display))
         {
@@ -356,39 +441,55 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        gettimeofday(&beginFrame, NULL);
         glClearColor(0.0, 0.0, 0.0, 0.0);
-        glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
+        // glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(id);
         glm::mat4 projection = glm::mat4(1.0f);
         // projection = glm::ortho(0.0f, 800.0f, 0.0f, 600.0f, 0.1f, 100.0f);
         projection = glm::perspective(glm::radians(45.0f), 4.0f/3.0f, 0.1f, 100.0f);
         glm::mat4 model = glm::mat4(1.0f);
 
         // printf("%f\n",(double) uptime);
-        model = glm::rotate(model, glm::radians(10.0f*clock()/CLOCKS_PER_SEC*1000), glm::vec3(1.0f, 1.0f, 0.0f));
+        // printf("%lu\n", );
+
+        model = glm::rotate(model, glm::radians((float) 10.0f*(get_milis()/100%10000)), glm::vec3(1.0f, 1.0f, 0.0f));
 
         glm::mat4 view= glm::mat4(1.0f);
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
-        glUniformMatrix4fv(glGetUniformLocation(id, "model"), 1, GL_FALSE, &model[0][0]);
-        glUniformMatrix4fv(glGetUniformLocation(id, "view"), 1, GL_FALSE, &view[0][0]);
-        glUniformMatrix4fv(glGetUniformLocation(id, "projection"), 1, GL_FALSE, &projection[0][0]);
+        glUseProgram(idCube);
+        glUniformMatrix4fv(glGetUniformLocation(idCube, "model"), 1, GL_FALSE, &model[0][0]);
+        glUniformMatrix4fv(glGetUniformLocation(idCube, "view"), 1, GL_FALSE, &view[0][0]);
+        glUniformMatrix4fv(glGetUniformLocation(idCube, "projection"), 1, GL_FALSE, &projection[0][0]);
 
-        glUseProgram(id);
-        glBindVertexArray(VAO);
+        // Draw Camera
+        glUseProgram(idCamera);
+        glBindVertexArray(cameraVAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+       
+        glClear(GL_DEPTH_BUFFER_BIT);
+        // // Draw Cube
+        glUseProgram(idCube);
+        glBindVertexArray(cubeVAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
         glBindVertexArray(0);
+
+
+
+        // Swap Buffers
         glXSwapBuffers(display, window);
 
         gettimeofday(&endFrame, NULL);
         ms = endFrame.tv_sec * 1000 + endFrame.tv_usec / 1000 -( beginFrame.tv_sec * 1000 + beginFrame.tv_usec / 1000);
         elapsed +=ms;
 
-        printf("elapsed: %f\n",(float) elapsed);
+        // printf("elapsed: %f\n",(float) elapsed);
 
-        printf("FPS: %f\n",(float) 1000/ms);
+        // printf("FPS: %f\n",(float) 1000/ms);
+        print_fps();
 
 
 
